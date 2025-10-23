db0e2fa3861da296ef57f4f12d541835c052bd18
roysh <rhaynes@sandiego.edu>
2025-10-22 20:30:12 -0700

    Feat: added create operation and local storage saving

--- js/model.js
+++ js/model.js
@@ -3,6 +3,36 @@ export class ChatModel{
         this.messages=this.loadMessages();
         this.observers=[];
     }
+
+    addObserver(callback){
+        this.observers.push(callback);
+    }
+
+    notifyObservers(){
+        this.observers.forEach(callback => callback());
+    }
+    addMessage(text, isUser){
+        const message= {
+            id: Date.now().toString(),
+            text: text,
+            isUser: isUser,
+            timestamp: Date.now().toString(),
+            edited: false
+        };
+        this.messages.push(message);
+        this.saveMessages();
+        this.notifyObservers();
+    }
+
+    saveMessages(){
+        try{
+            localStorage.setItem('chatMessages', JSON.stringify(this.messages));
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
     loadMessages(){
         try{
             const data = localStorage.getItem("chatMessages");
@@ -12,11 +42,5 @@ export class ChatModel{
             return [];
         }
     }
-    addObserver(callback){
-        this.observers.push(callback);
-    }
 
-    notifyObservers(){
-        this.observers.forEach(callback => callback());
-    }
 }
