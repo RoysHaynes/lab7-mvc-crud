ca8f0fb32b296a0731a735e2c410d361a73723b2
roysh <rhaynes@sandiego.edu>
2025-10-22 20:16:16 -0700

    Feat: added local storage loading to chat model

--- js/model.js
+++ js/model.js
@@ -2,4 +2,13 @@ export class ChatModel{
     constructor(){
         this.message=[];
     }
+    loadMessages(){
+        try{
+            const data = localStorage.getItem("chatMessages");
+            return data ? JSON.parse(data) : [];
+        }catch(error){
+            console.error(error);
+            return [];
+        }
+    }
 }
